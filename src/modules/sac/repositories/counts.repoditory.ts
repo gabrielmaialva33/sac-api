beaaff73219006be6c738d83ad1c36bcb6c8d567
Gabriel Maia <gabrielmaialva33@gmail.com>
2021-12-17 19:00:49 -0300

    feat: implement index for count

--- src/modules/sac/repositories/counts.repoditory.ts
+++ src/modules/sac/repositories/counts.repoditory.ts
@@ -16,9 +16,14 @@ export default class CountsRepository implements ICount.Repository {
   public async index(params: ICount.DTO.Index): Promise<PaginationAwareObject> {
     const { start_date, end_date, granularity, issue_id } = params;
 
-    const query = `select i "date_time", ( select count(*) from counts as c where c.issue_id = $1 and c.created_at < i + interval '1 ${granularity}' and c.created_at > i ) "count" from generate_series($2, $3, '1 ${granularity}'::interval) i;`;
+    const query_filter = `select i "date_time", ( select count(*) from counts as c where c.issue_id = $1 and c.created_at < i + interval '1 ${granularity}' and c.created_at > i ) "count" from generate_series($2, $3, '1 ${granularity}'::interval) i;`;
+    //const query_all = `select i "date_time", ( select count(*) from counts as c where c.issue_id = $1 and  i + interval '1 ${granularity}') "count" from generate_series($2, $3, '1 ${granularity}'::interval) i;`;
 
-    return this.ormManager.query(query, [issue_id, start_date, end_date]);
+    return this.ormManager.query(query_filter, [
+      issue_id,
+      start_date,
+      end_date
+    ]);
   }
 
   public async store(data: ICount.DTO.Store): Promise<Count> {
--- src/modules/sac/services/count/index.count.service.ts
+++ src/modules/sac/services/count/index.count.service.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-import { endOfHour, startOfHour } from 'date-fns';
+import { startOfDay, endOfDay } from 'date-fns';
 import { SetGranularity } from '@modules/sac/utils';
 
 import { ICount } from '@modules/sac/interfaces/icounts.interfaces';
@@ -23,16 +23,16 @@ export class IndexCountService {
       const { start_date, end_date, granularity, issue_id }: ICount.DTO.Index =
         {
           start_date: data.start_date
-            ? new Date(data.start_date)
-            : new Date(startOfHour(new Date())),
+            ? new Date(String(data.start_date))
+            : new Date(String(startOfDay(new Date()))),
           end_date: data.end_date
-            ? new Date(data.end_date)
-            : new Date(endOfHour(new Date())),
+            ? new Date(String(data.end_date))
+            : new Date(String(endOfDay(new Date()))),
           granularity: data.granularity
             ? data.granularity
             : SetGranularity(
-                new Date(data.start_date),
-                new Date(data.end_date)
+                new Date(String(data.start_date)),
+                new Date(String(data.end_date))
               ),
           issue_id: data.issue_id
         };
